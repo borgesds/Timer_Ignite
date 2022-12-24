a1feab0cfe3355a0ac3b085b46f001410fcb377b
André Borges <borges.an@hotmail.com>
2022-12-24 09:47:54 -0300

    Up Code

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-
-import { Router } from './Router'
 import { CyclesContextProvider } from './contexts/CyclesContext'
+import { Router } from './Router'
 
 export function App() {
   return (
